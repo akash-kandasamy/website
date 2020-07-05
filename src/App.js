92bd37c7ffeb7be0dbbdac9a3af7dd673bae2fbe
akash-kandasamy <akashram953@gmail.com>
2020-07-05 11:54:18 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div className="navbar">
+            <h2 className='title'>Devi's Kitchen</h2>
+            <div className='space'></div>
+            <nav>
+              <ul className="list">
+                <li className="listItem">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="listItem">
+                  <Link to="/about">About</Link>
+                </li>
+                <li className="listItem">
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+            </nav>
+        </div>
+
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
